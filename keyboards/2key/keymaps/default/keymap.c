a205f3ab53d4e101d710cba71f4e4c1bcc99b8a3
Carlos Filoteo <c.filoteo@live.com>
2018-01-20 23:56:49 -0700

    Implement custom matrix, remove tapdance

--- keyboards/2key/keymaps/default/keymap.c
+++ keyboards/2key/keymaps/default/keymap.c
@@ -1,10 +1,5 @@
 #include "2key.h"
-
-// Tap Dance Declarations
-enum {
-  LEFT_UP,
-  RIGHT_DOWN
-};
+#include "matrix.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -15,11 +10,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   {{KC_A, KC_B}}
 };
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [LEFT_UP] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_UP),
-  [RIGHT_DOWN] = ACTION_TAP_DANCE_DOUBLE(KC_RIGHT, KC_DOWN)
-};
-
-void matrix_init_user(void) {
-}
