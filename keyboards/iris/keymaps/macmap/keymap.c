9505804459e5312cb40a261ff09704d3619a0abb
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-06-01 11:36:39 -0600

    Update Iris keymap

--- keyboards/iris/keymaps/macmap/keymap.c
+++ keyboards/iris/keymaps/macmap/keymap.c
@@ -16,69 +16,66 @@ enum custom_keycodes {
   ADJUST,
 };
 
-#define KC_ KC_TRNS
 #define TRNS KC_TRNS
-
-#define KC_LOWR LOWER
-#define KC_RASE RAISE
+#define ___ KC_TRNS
 #define KC_RST RESET
 #define KC_BL_S BL_STEP
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_kc(
+  [_QWERTY] = LAYOUT(
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      GESC ,   1   ,   2   ,   3   ,   4   ,   5   ,                     6   ,   7   ,   8   ,   9   ,   0   ,  BSPC ,
+    KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                   KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TAB  ,   Q   ,   W   ,   E   ,   R   ,   T   ,                     Y   ,   U   ,   I   ,   O   ,   P   ,  BSLS ,
+    KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                   KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLS ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      LSFT ,   A   ,   S   ,   D   ,   F   ,   G   ,                     H   ,   J   ,   K   ,   L   ,  SCLN ,  QUOT ,
+    KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                   KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT ,
   //|------|-------|-------|-------|-------|-------|-------   -------|-------|-------|-------|-------|-------|-------|
-      LCTL ,  LALT ,   Z   ,   X   ,   C   ,   V   ,  RSFT ,    LSFT ,   B   ,   N   ,   M   ,  COMM ,  DOT  ,  SLSH ,
+    KC_LCTL,KC_LALT, KC_Z  , KC_X  , KC_C  , KC_V  ,KC_RSFT,  KC_LSFT, KC_B  , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-                                     LOWR  , LGUI  , BSPC ,      SPC ,  ENT  ,  RASE
+                                     LOWER ,KC_LGUI,KC_BSPC,   KC_SPC, KC_ENT, RAISE
   //                               .-------|-------/-----/     \-----|-------|-------.
   ),
 
-  [_LOWER] = LAYOUT_kc(
+  [_LOWER] = LAYOUT(
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TILD , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,  TRNS ,  TRNS ,  MINS ,  EQL  ,  DEL  ,
+    KC_TILD,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,                    ___  ,  ___  ,  ___  ,KC_MINS, KC_EQL, KC_DEL,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,  TRNS ,  TRNS ,  LCBR ,  RCBR ,  TRNS ,
+      ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,                    ___  ,  ___  ,  ___  ,KC_LBRC,KC_RBRC,  ___  ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,                    LEFT ,  DOWN ,   UP  , RIGHT ,  TRNS ,  TRNS ,
+      ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,                  KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,  ___  ,  ___  ,
   //|------|-------|-------|-------|-------|-------|-------   -------|-------|-------|-------|-------|-------|-------|
-      TRNS , TRNS  , TRNS  , TRNS  , TRNS  , TRNS  , TRNS ,     TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
+      ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,    ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-                                     TRNS  , TRNS  , TRNS ,     TRNS ,  TRNS ,  TRNS 
+                                      ___  ,  ___  ,  ___  ,    ___  ,  ___  ,  ___   
   //                               .-------|-------/-----/     \-----|-------|-------.
   ),
 
-  [_RAISE] = LAYOUT_kc(
+  [_RAISE] = LAYOUT(
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-       F1  ,   F2  ,   F3  ,   F4  ,   F5  ,   F6  ,                    F7   ,  F8   ,  F9   ,  F10  ,  F11  ,  F12  ,
+     KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                   KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11, KC_F12,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TRNS ,  TRNS ,  TRNS ,  F14  ,  F15  ,  TRNS ,                    MPRV ,  MPLY ,  MNXT ,  MUTE ,  VOLD ,  VOLU ,
+      ___  ,  ___  ,  ___  ,KC_F14 ,KC_F15 ,  ___  ,                  KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-      TRNS ,  TRNS ,  TRNS ,  TRNS ,  WBAK ,  WFWD ,                    HOME ,  PGUP ,  PGDN ,  END  ,  TRNS ,  TRNS ,
+      ___  ,  ___  ,  ___  ,  ___  ,KC_WBAK,KC_WFWD,                  KC_HOME,KC_PGUP,KC_PGDN,KC_END ,  ___  ,  ___  ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-      TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,     TRNS ,  TRNS ,  TRNS  , TRNS ,  TRNS ,  TRNS ,  TRNS , 
+      ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,    ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,  ___  , 
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-                                      TRNS ,  TRNS , DEL ,     TRNS ,  TRNS , TRNS
+                                      ___  ,  ___  ,KC_DEL,     ___  ,  ___  ,  ___  
   //                               .-------|-------/-----/     \-----|-------|-------.
   ),
 
   [_ADJUST] = LAYOUT(
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-     RESET , DEBUG , TRNS  , TRNS  , TRNS  , TRNS  ,                    TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS ,
+     RESET , DEBUG ,  ___  ,  ___  ,  ___  ,  ___  ,                     ___ ,   ___ ,   ___ ,   ___ ,   ___ ,   ___ ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-     TRNS  ,RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,  TRNS ,                    TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,  TRNS ,
+      ___  ,RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,  ___  ,                     ___ ,   ___ ,   ___ ,   ___ ,  ___  ,   ___ ,
   //|------|-------|-------|-------|-------|-------.                 .-------|-------|-------|-------|-------|-------|
-     TRNS  ,RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,  TRNS ,                    TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,  TRNS ,
+      ___  ,RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,  ___  ,                     ___ ,   ___ ,   ___ ,   ___ ,  ___  ,   ___ ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-     TRNS  ,KC_ASTG, TRNS  , TRNS  , TRNS  , TRNS  , TRNS  ,    TRNS ,  TRNS ,  TRNS ,  TRNS ,  TRNS , TRNS ,  TRNS ,
+      ___  ,KC_ASTG,  ___  ,  ___  ,  ___  ,  ___  ,  ___  ,     ___ ,   ___ ,   ___ ,   ___ ,   ___ ,  ___  ,   ___ ,
   //|------|-------|-------|-------|-------|-------|------/   \------|-------|-------|-------|-------|-------|-------|
-                                      TRNS ,  TRNS , TRNS ,     TRNS ,  TRNS ,  TRNS
+                                       ___ ,   ___ ,  ___ ,      ___ ,   ___ ,   ___
   //                               .-------|-------/-----/     \-----|-------|-------.
   )
 
