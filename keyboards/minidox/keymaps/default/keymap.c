8743f6d9fac2cbd21c36ae7efb7d34344f202932
Carlos Filoteo <c.filoteo@live.com>
2018-09-16 13:15:52 -0600

    Update minidox layotu

--- keyboards/minidox/keymaps/default/keymap.c
+++ keyboards/minidox/keymaps/default/keymap.c
@@ -25,9 +25,8 @@ enum custom_keycodes {
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-// Defines for task manager and such
-#define CALTDEL LCTL(LALT(KC_DEL))
-#define TSKMGR LCTL(LSFT(KC_ESC))
+#define GUI_BSP GUI_T(KC_BSPC)
+#define SFT_SPC SFT_T(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -41,16 +40,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  | Ctrl | LOWER|      |    |      | RAISE| Shift|
- *                  `-------------| Space|    |BckSpc|------+------.
- *                                |      |    |      |
+ *                  | LOWER| GUI  |      |    |      | Enter| RAISE|
+ *                  `-------------|BckSpc|    |Space |------+------.
+ *                                |/GUI  |    |/Shift|
  *                                `------'    `------'
  */
 [_QWERTY] = LAYOUT( \
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
+                    LOWER, KC_LGUI,GUI_BSP,         SFT_SPC, KC_ENT,  RAISE                 \
 ),
 
 /* Raise
@@ -70,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT, \
+  KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
+  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, \
                     _______, _______, _______,      _______, _______, _______                    \
 ),
 
@@ -114,8 +113,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, TSKMGR, CALTDEL, \
-  RESET,   _______, _______, _______, _______,      _______, _______, _______, _______,  _______, \
+  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, _______, _______, \
+  RESET,   _______, _______, _______, _______,      _______, _______, _______, _______, _______, \
                     _______, _______, _______,      _______,  _______, _______                    \
 )
 };
