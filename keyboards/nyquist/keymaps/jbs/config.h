cfc232c4dd5e3717abbbbe50a021cfff2dea18e8
Carlos Filoteo <c.filoteo@live.com>
2018-10-17 22:17:32 -0600

    Do not disable LAYER actions

--- keyboards/nyquist/keymaps/jbs/config.h
+++ keyboards/nyquist/keymaps/jbs/config.h
@@ -41,7 +41,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* disable action features */
-#define NO_ACTION_LAYER
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
