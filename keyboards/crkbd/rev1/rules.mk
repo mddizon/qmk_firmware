dd1d78e4fc7c7e699e119e1df2acf80b57e22c70
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-20 14:42:50 -0600

    Initial layout

--- keyboards/crkbd/rev1/rules.mk
+++ keyboards/crkbd/rev1/rules.mk
@@ -1,4 +1,3 @@
-RGB_MATRIX_SPLIT_RIGHT = no  # if no, order LEDs for left hand, if yes, order LEDs for right hand
 
 ifeq ($(strip $(RGB_MATRIX_SPLIT_RIGHT)), yes)
     OPT_DEFS += -DRGB_MATRIX_SPLIT_RIGHT
