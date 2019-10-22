e3027c0b571c6f68c7b36300d7e4cd21a48cbcd1
Carlos Filoteo <c.filoteo12@gmail.com>
2019-10-22 00:07:03 -0600

    Add working crkbd matrix layout

---
+++ keyboards/crkbd/keymaps/filoxo/rules.mk
@@ -0,0 +1,15 @@
+MOUSEKEY_ENABLE = no
+RGB_MATRIX_ENABLE = WS2812
+TAP_DANCE_ENABLE=yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./keymaps/filoxo/glcdfont.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        # ./lib/keylogger.c \
+        # ./lib/rgb_state_reader.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
+
+EXTRAFLAGS += -flto
